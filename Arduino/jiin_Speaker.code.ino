4a6f302a02f7c53451e42478fb03674b8a732e27
tlswldls <42241350+tlswldls@users.noreply.github.com>
2019-08-29 01:31:17 +0900

    Update jiin_Speaker
    아두이노 스피커 모듈 공부하는 파일입니다. 곰세마리와 학교종이 땡땡땡 멜로디와 템포 추가했습니다.

--- Arduino/jiin_Speaker.code.ino
+++ Arduino/jiin_Speaker.code.ino
@@ -1,15 +1,75 @@
-void setup()
-{
-  int melody[] = {262, 294, 330, 349, 392, 440, 492};  
-  for(int i = 0; i<7; i++){
-    tone(8, melody[i], 500); delay(500);
-  }
+/*
+    Speaker
+*/
+
+#define NOTE_C5  523   //do
+#define NOTE_D5  587   //re
+#define NOTE_E5  659   //mi
+#define NOTE_F5  698   //pa
+#define NOTE_G5  784   //sol
+#define NOTE_A5  880   //la
+#define NOTE_B5  988   //si
+#define NOTE_C6  1047  //do
+
+int tonepin = 8;
+
+//3bears
+int melody_3bears[] = {
+NOTE_C5,NOTE_C5,NOTE_C5,NOTE_C5,NOTE_C5,               //?????
+NOTE_E5,NOTE_G5,NOTE_G5,NOTE_E5,NOTE_C5,               //?????
+NOTE_G5,NOTE_G5,NOTE_E5,NOTE_G5,NOTE_G5,NOTE_E5,       //??????
+NOTE_C5,NOTE_C5,NOTE_C5,                               //???
+NOTE_G5,NOTE_G5,NOTE_E5,NOTE_C5,                       //????
+NOTE_G5,NOTE_G5,NOTE_G5,                               //???
+NOTE_G5,NOTE_G5,NOTE_E5,NOTE_C5,                       //????
+NOTE_G5,NOTE_G5,NOTE_G5,                               //???
+NOTE_G5,NOTE_G5,NOTE_E5,NOTE_C5,                       //????
+NOTE_G5,NOTE_G5,NOTE_G5,NOTE_A5,NOTE_G5,               //?????
+NOTE_C6,NOTE_G5,NOTE_C6,NOTE_G5,                       //????
+NOTE_E5,NOTE_D5,NOTE_C5                                //???
+};
+int tempo_3bears[]={
+4,8,8,4,4,
+4,8,8,4,4,
+8,8,4,8,8,4,
+4,4,2,
+4,4,4,4,
+4,4,2,
+4,4,4,4,
+4,4,2,
+4,4,4,4,
+8,8,8,8,2,
+4,4,4,4,
+4,4,2
+};
+
+//school bell
+int melody_school[]={
+NOTE_G5, NOTE_G5, NOTE_A5, NOTE_A5, NOTE_G5, NOTE_G5, NOTE_E5   //???????
+NOTE_G5, NOTE_G5, NOTE_E5, NOTE_E5, NOTE_D5                     //?????
+NOTE_G5, NOTE_G5, NOTE_A5, NOTE_A5, NOTE_G5, NOTE_G5, NOTE_E5   //???????
+NOTE_G5, NOTE_E5, NOTE_D5, NOTE_E5,NOTE_C5                      //?????
 }
 
-void loop()
-{
-  digitalWrite(13, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(13, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
+
+
+void setup() { 
+} 
+void loop() {
+
+int music[];
+int tempo[];
+
+//choose the music to play()data from android
+switch
+
+//play the music
+  for (int i = 0; i < sizeof(music); i++) {
+    int Durations = 1000/tmepo[i];    // calculate sound length
+    tone(tonepin, music[i], Durations);    
+    int pauseBetweenNotes = Durations *1.3 ;
+    delay(pauseBetweenNotes);
+    noTone(tonepin);
+  }
+
 }
