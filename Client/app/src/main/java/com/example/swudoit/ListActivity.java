36cdb6aea6b90a1a6b45e97dc60ad0185a9a6558
kwldusrnclf <kwldusrnclf97@gmail.com>
2019-08-30 14:19:02 +0900

    다이어리 test

---
+++ Client/app/src/main/java/com/example/swudoit/ListActivity.java
@@ -0,0 +1,20 @@
+package com.example.swudoit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListActivity extends AppCompatActivity {
+
+    int count;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+
+
+
+    }
+}
