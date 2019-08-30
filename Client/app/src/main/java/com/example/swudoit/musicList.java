fa5a6dcdcd7ed10bf62fc7a580fae4cf46b83045
kwldusrnclf <kwldusrnclf97@gmail.com>
2019-08-30 21:12:09 +0900

    블루투스 디버깅용

--- Client/app/src/main/java/com/example/swudoit/musicList.java
+++ Client/app/src/main/java/com/example/swudoit/musicList.java
@@ -2,6 +2,8 @@ package com.example.swudoit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +12,7 @@ import android.widget.ImageButton;
 import android.widget.Toast;
 import com.example.swudoit.fragment.FragmentHome;
 import app.akexorcist.bluetotohspp.library.BluetoothSPP;
+import app.akexorcist.bluetotohspp.library.BluetoothState;
 
 public class musicList extends AppCompatActivity {
     private ImageButton music1;
@@ -26,36 +29,36 @@ public class musicList extends AppCompatActivity {
         /*블루투스*/
         bt = new BluetoothSPP(this); //Initializing
 
-        if (!bt.isBluetoothAvailable()) { //블루투스 사용 불가
-            Toast.makeText(getApplicationContext()
-                    , "Bluetooth is not available"
-                    , Toast.LENGTH_SHORT).show();
-            finish();
-        }
-
-        bt.setOnDataReceivedListener(new BluetoothSPP.OnDataReceivedListener() { //데이터 수신
-            public void onDataReceived(byte[] data, String message) {
-                Toast.makeText(musicList.this, message, Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        bt.setBluetoothConnectionListener(new BluetoothSPP.BluetoothConnectionListener() { //연결됐을 때
-            public void onDeviceConnected(String name, String address) {
-                Toast.makeText(getApplicationContext()
-                        , "Connected to " + name + "\n" + address
-                        , Toast.LENGTH_SHORT).show();
-            }
-
-            public void onDeviceDisconnected() { //연결해제
-                Toast.makeText(getApplicationContext()
-                        , "Connection lost", Toast.LENGTH_SHORT).show();
-            }
-
-            public void onDeviceConnectionFailed() { //연결실패
-                Toast.makeText(getApplicationContext()
-                        , "Unable to connect", Toast.LENGTH_SHORT).show();
-            }
-        }); //end 블루투스
+//        if (!bt.isBluetoothAvailable()) { //블루투스 사용 불가
+//            Toast.makeText(getApplicationContext()
+//                    , "Bluetooth is not available"
+//                    , Toast.LENGTH_SHORT).show();
+//            finish();
+//        }
+//
+//        bt.setOnDataReceivedListener(new BluetoothSPP.OnDataReceivedListener() { //데이터 수신
+//            public void onDataReceived(byte[] data, String message) {
+//                Toast.makeText(musicList.this, message, Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        bt.setBluetoothConnectionListener(new BluetoothSPP.BluetoothConnectionListener() { //연결됐을 때
+//            public void onDeviceConnected(String name, String address) {
+//                Toast.makeText(getApplicationContext()
+//                        , "Connected to " + name + "\n" + address
+//                        , Toast.LENGTH_SHORT).show();
+//            }
+//
+//            public void onDeviceDisconnected() { //연결해제
+//                Toast.makeText(getApplicationContext()
+//                        , "Connection lost", Toast.LENGTH_SHORT).show();
+//            }
+//
+//            public void onDeviceConnectionFailed() { //연결실패
+//                Toast.makeText(getApplicationContext()
+//                        , "Unable to connect", Toast.LENGTH_SHORT).show();
+//            }
+//        }); //end 블루투스
 
 
         music1 = (ImageButton)findViewById(R.id.musicPlayer);
@@ -96,6 +99,26 @@ public class musicList extends AppCompatActivity {
         });
 
     }//end onCreate
+
+//    public void onDestroy() {
+//        super.onDestroy();
+//        bt.stopService(); //블루투스 중지
+//    }
+//
+//    public void onStart() {
+//        super.onStart();
+//        if (!bt.isBluetoothEnabled()) { //
+//            Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+//            startActivityForResult(intent, BluetoothState.REQUEST_ENABLE_BT);
+//        } else {
+//            if (!bt.isServiceAvailable()) {
+//                bt.setupService();
+//                bt.startService(BluetoothState.DEVICE_OTHER); //DEVICE_ANDROID는 안드로이드 기기 끼리
+//                setup();
+//            }
+//        }
+//    }
+
 
     public void setup() {
         //곰세마리
