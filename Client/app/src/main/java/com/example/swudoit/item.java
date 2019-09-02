9b6232bd329274c2b1c88f7e80f2f3cd58281ecd
YeRamJung <jyr2356@gmail.com>
2019-09-03 08:13:43 +0900

    item.java push

--- Client/app/src/main/java/com/example/swudoit/item.java
+++ Client/app/src/main/java/com/example/swudoit/item.java
@@ -23,38 +23,10 @@ import java.util.List;
 
 public class item extends AppCompatActivity {
 
-    TextView itemTitle;
-    TextView itemContent;
-    TextView itemToday;
-    ImageView itemImage;
-
-    List<ItemBean> listView;
-
-    String title;
-    String content;
-    String today;
-    String image_name;
-    String image_copied;
-
-    Adapter adapter;
-
-    Diary d = new Diary();
-    FragmentDiary fd = new FragmentDiary();
-    ItemAdapter iad = new ItemAdapter();
-    int index = fd.index;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item);
-
-        itemTitle = (TextView)findViewById(R.id.itemTitle);
-        itemContent = (TextView)findViewById(R.id.itemContent);
-        itemToday = (TextView)findViewById(R.id.itemToday);
-        itemImage = (ImageView)findViewById(R.id.itemImage);
-
-        iad.setList(listView);
 
     }
 
